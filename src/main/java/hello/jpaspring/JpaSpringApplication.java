38be7c4691f5e4766dbd258f806574cd5cd59d4c
cvjeongwon <cvjeongwon@gmail.com>
2024-03-31 12:15:12 +0900

    1.  Spring Boot 프로젝트 생성후  SpringeduApplication 실행시 첫번쩨 에러 해결 >  @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
    2. tomcat port 충돌시
    > src/main/resources/application.properties 파일에 tomcat port 를 8088
    spring.application.name=jpa-spring
    # server.port=8088

--- src/main/java/hello/jpaspring/JpaSpringApplication.java
+++ src/main/java/hello/jpaspring/JpaSpringApplication.java
@@ -2,8 +2,9 @@ package hello.jpaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class JpaSpringApplication {
 
     public static void main(String[] args) {
